7b21dd93e136bc406dd43d1690fd7c2575c0b687
Laplang446 <107035060+Laplang446@users.noreply.github.com>
2022-10-25 03:12:52 +0700

    up

--- Login.php
+++ Login.php
@@ -1,9 +1,10 @@
-<link rel="stylesheet" type="text/css" href="style.css"/>
+<!-- <link rel="stylesheet" type="text/css" href="style.css"/>
 <link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="css/responsive.css">
 <script src="js/jquery-3.2.0.min.js"/></script>
 <script src="js/jquery.dataTables.min.js"/></script>
-<script src="js/dataTables.bootstrap.min.js"/></script>
+<script src="js/dataTables.bootstrap.min.js"/></script> -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <?php
     if (isset($_POST['btnLogin']))
 	{
@@ -45,11 +46,11 @@
 ?>
 
 <h1>Login</h1>
-<form id="form1" name="form1" method="POST" action="">
+<!-- <form id="form1" name="form1" method="POST" action="">
 <div class="row">
     <div class="form-group"  >				    
         <label for="txtUsername" class="col-sm-2 control-label">Username(*):   </label>
-		<div class="col-sm-10">
+		<div class="col-sm-10"></div>
 		     <br> <input type="text" name="txtUsername" id="txtUsername" class="form-control" placeholder="Username" value=""/>
 		</div>
       </div>  
@@ -68,7 +69,20 @@
             <input type="submit" name="btnCancel"  class="btn btn-primary" id="btnLogin" value="Cancel"/>
 		</div>  
 	</div>
+
  
 
+</form> -->
+<form id="form1" name="form1" method="POST" action="">
+  <div class="mb-3">
+   
+    <input type="text" class="form-control" name="txtUsername" id="txtUsername" aria-describedby="emailHelp" placeholder="Username" value="">
+   
+  </div>
+  <div class="mb-3">
+    <input type="password"  name="txtPass" class="form-control" id="txtPass" placeholder="Password" value="">
+  </div>
+  <input type="submit" name="btnLogin"  class="btn btn-primary" id="btnLogin" value="Login"/>
+  <input type="submit" name="btnCancel"  class="btn btn-primary" id="btnLogin" value="Cancel"/>
 </form>
   
